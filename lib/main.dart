12b06b2190b45e08414c457d39369bd481611480
sourav santra <souravsantra59580@gmail.com>
2020-07-08 00:52:21 +0530

    second commit(08/07/2020)

--- lib/main.dart
+++ lib/main.dart
@@ -27,12 +27,74 @@ class _HomePageState extends State<HomePage> {
             onPressed: (){},
           ),
         ],
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.pink,
       ),
       drawer: Drawer(
         child: new ListView(
           children: <Widget>[
-            
+            //header part of drawer
+            UserAccountsDrawerHeader(
+                accountName: Text("Sourav Santra"),
+                accountEmail: Text("souravsantra59580@gmail.com"),
+            currentAccountPicture: GestureDetector(
+              child: CircleAvatar(
+                child: Icon(Icons.person,color: Colors.black,),
+                backgroundColor: Colors.yellowAccent,
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.pink
+            ),),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                leading: Icon(Icons.home,color: Colors.red,),
+                title: Text("Home"),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                leading: Icon(Icons.person,color: Colors.red,),
+                title: Text("My account"),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                leading: Icon(Icons.shopping_basket,color: Colors.red,),
+                title: Text("My orders"),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                leading: Icon(Icons.dashboard,color: Colors.red,),
+                title: Text("Categoris"),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                leading: Icon(Icons.favorite,color: Colors.red,),
+                title: Text("Favourites"),
+              ),
+            ),
+            Divider(),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                leading: Icon(Icons.settings,color: Colors.red,),
+                title: Text("Setting"),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                leading: Icon(Icons.help,color: Colors.red,),
+                title: Text("About"),
+              ),
+            )
           ],
         ),
       ),
